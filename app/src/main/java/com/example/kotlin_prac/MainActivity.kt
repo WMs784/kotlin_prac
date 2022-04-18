6f3009643d4032909b57dac0d6d7a4f88d5bd3c2
WMs784 <wms784.app@gmail.com>
2022-04-18 10:55:06 +0900

    initial commit

---
+++ app/src/main/java/com/example/kotlin_prac/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kotlin_prac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+    fun buttonClicked(view: View) {
+        Toast.makeText(this, "Hello, world", Toast.LENGTH_LONG).show()
+    }
+}
